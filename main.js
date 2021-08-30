9c7bc9d25567441670e0e2b752209b67630690e4
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-08-30 12:18:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightwristX=0;
+leftwristX=0;
+
+
+function setup(){
+    video  = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized!');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = "+ noseX + "nose y = "+ noseY);
+
+        leftwristX=results[0].pose.leftWrist.x;
+      rightwristX=results[0].pose.rightWrist.x;
+      difference =floor( leftwristX - rightwristX); 
+      
+      console.log("left wrist x = "+ leftwristX+"right wrist x = " + rightwristX + "difference = "+difference);
+    }
+}
+function draw(){
+    background('#666666');
+
+    document.getElementById("squre_side").innerHTML = "Width and Height of text will be "+difference+"px";
+    fill('#000066');
+    stroke('#000066');
+    text("arnavkh21",noseX,noseY);
+    textSize(difference);
+}
+
+
